670361af70b4e614fd06db278696d907bb036153
Adellar Irankunda <addyirankunda@gmail.com>
2024-03-03 17:16:56 -0800

    Multiple spheres

--- Rendering/Assets/RaytraceMaster.cs
+++ Rendering/Assets/RaytraceMaster.cs
@@ -1,30 +1,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RaytraceMaster : MonoBehaviour
 {
     public RenderTexture target;
     public ComputeShader tracer;
     public Texture Skybox;
+    
+    [Range(1, 20)]
+    public int SphereCount = 5;
     struct DispatchParams
     {
         public int x;
         public int y;
         public int z;
     }
-    
+
+    struct Sphere
+    {
+        public Vector4 point;
+        public Vector3 specular;
+        public Vector3 albedo;
+    }
     
     private DispatchParams groups;
     private Camera main;
-    
-    
+
+    private ComputeBuffer Spheres;
     
     void Start()
     {
         main = Camera.main;
         PrimeTarget();
+        CreateSpheres();
     }
 
     void PrimeTarget()
@@ -39,13 +51,38 @@ public class RaytraceMaster : MonoBehaviour
         groups.z = 0;
     }
 
+    void CreateSpheres()
+    {
+        Sphere[] spheres = new Sphere[SphereCount];
+        for (int a = 0; a < SphereCount; a++)
+        {
+            Sphere s = new Sphere();
+            float radius = Random.Range(0.1f, 1f);
+
+            var xz = Random.insideUnitCircle * 5f; //space them out because they have radius
+            var spec = Random.insideUnitSphere;
+            var alb = Random.insideUnitSphere;
+
+            s.point = new Vector4(xz.x, radius, xz.y, radius);
+            s.specular = spec;
+            s.albedo = alb;
+
+            spheres[a] = s;
+        }
+
+        Spheres = new ComputeBuffer(SphereCount, Marshal.SizeOf(typeof(Sphere)));
+        Spheres.SetData(spheres);
+    }
+
     void CallTrace()
     {
         tracer.SetTexture(0, "Result", target);
         tracer.SetMatrix("CameraToWorld", main.cameraToWorldMatrix);
         tracer.SetMatrix("WorldToCamera", main.worldToCameraMatrix);
         tracer.SetMatrix("CameraInverseProjection", main.projectionMatrix.inverse);
+        tracer.SetBuffer(0, "Spheres", Spheres);
         tracer.SetTexture(0, "Skybox", Skybox);
+        tracer.SetInt("SphereCount", SphereCount);
         tracer.Dispatch(0, groups.x, groups.y, 1);
     }
 
