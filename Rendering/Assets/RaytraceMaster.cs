098034d17775cea0a1f832ca51df0d99601120f1
Adellar Irankunda <addyirankunda@gmail.com>
2024-03-17 20:06:03 -0400

    Added TSAA

--- Rendering/Assets/RaytraceMaster.cs
+++ Rendering/Assets/RaytraceMaster.cs
@@ -33,7 +33,8 @@ public class RaytraceMaster : MonoBehaviour
     private Camera main;
     private Light dirLight;
     private ComputeBuffer Spheres;
-    
+    private uint _currentSample = 0;
+    private Material _addMaterial;
     void Start()
     {
         main = Camera.main;
@@ -52,6 +53,8 @@ public class RaytraceMaster : MonoBehaviour
         groups.x = Mathf.CeilToInt(main.pixelWidth / 8);
         groups.y = Mathf.CeilToInt(main.pixelHeight / 8);
         groups.z = 0;
+
+        
     }
 
     void CreateSpheres()
@@ -87,20 +90,30 @@ public class RaytraceMaster : MonoBehaviour
         tracer.SetBuffer(0, "Spheres", Spheres);
         tracer.SetTexture(0, "Skybox", Skybox);
         tracer.SetInt("SphereCount", SphereCount);
+        tracer.SetVector("CameraPosition", transform.position);
         tracer.SetVector("light", new Vector4(mainLight.forward.x, mainLight.forward.y, mainLight.forward.z, dirLight.intensity));
+        tracer.SetVector("_Pixel", new Vector4(0, 0, Random.value, Random.value));
         tracer.Dispatch(0, groups.x, groups.y, 1);
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         CallTrace();
-        Graphics.Blit(target, destination);
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/TSAA"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(target, destination, _addMaterial);
+        _currentSample++;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
     }
 
     private void OnApplicationQuit()
