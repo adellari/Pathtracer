42e740a2e1a95979ccd351209996d4b50a3d4f52
Adellar Irankunda <addyirankunda@gmail.com>
2024-03-24 01:35:04 -0400

    added emission and fixed energy non convergence

--- Rendering/Assets/RaytraceMaster.cs
+++ Rendering/Assets/RaytraceMaster.cs
@@ -28,6 +28,7 @@ public class RaytraceMaster : MonoBehaviour
         public Vector4 point;
         public Vector3 specular;
         public Vector3 albedo;
+        public Vector3 emission;
     }
     
     private DispatchParams groups;
@@ -47,9 +48,12 @@ public class RaytraceMaster : MonoBehaviour
     void PrimeTarget()
     {
         target = new RenderTexture(main.pixelWidth, main.pixelHeight, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
-        converged = new RenderTexture(main.pixelWidth, main.pixelHeight, 0, RenderTextureFormat.DefaultHDR);
         target.enableRandomWrite = true;
         target.Create();
+        
+        converged = new RenderTexture(main.pixelWidth, main.pixelHeight, 0, RenderTextureFormat.DefaultHDR, RenderTextureReadWrite.Linear);
+        converged.enableRandomWrite = true;
+        converged.Create();
 
         groups = new DispatchParams();
         groups.x = Mathf.CeilToInt(main.pixelWidth / 8);
@@ -71,7 +75,17 @@ public class RaytraceMaster : MonoBehaviour
             //var spec = Random.insideUnitSphere;
             var alb = Random.ColorHSV();
             bool metallic = Random.value < 0.5f;
+            var chance = Random.value;
 
+            if (chance > 0.8f)
+            {
+                var col = Random.ColorHSV(0, 1, 0, 1, 3.0f, 8.0f);
+                s.emission = new Vector3(col.r, col.g, col.b);
+            }
+            else
+            {
+                s.emission = new Vector3();
+            }
             
             s.point = new Vector4(xz.x, radius, xz.y, radius);
             s.specular = metallic ? new Vector3(alb.r, alb.g, alb.b) : Vector3.one * 0.04f;
